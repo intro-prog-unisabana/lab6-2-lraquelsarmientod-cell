17a9e803089dd1ad95887abc1960b48b5bc34a66
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-01 11:54:43 -0500

    3

--- dict_func.py
+++ dict_func.py
@@ -1,2 +1,22 @@
 # Write your code here!
-def employee_print(employe_info):
+def employee_print(employee_info):
+    # Sacamos los valores base (o N/A si no existen)
+    name = employee_info.get("Name", "N/A")
+    salary = employee_info.get("Salary", "N/A")
+    role = employee_info.get("Role", "N/A")
+
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+
+    extra_info = employee_info.copy()
+
+    extra_info.pop("Name", None)
+    extra_info.pop("Salary", None)
+    extra_info.pop("Role", None)
+
+    if len(extra_info) == 0:
+        print("No other info!")
+    else:
+        for key, value in extra_info.items():
+            print(f"{key}: {value}")
