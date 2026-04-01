9b697acf793a45bc6d32b577c06388ff6465cf16
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-01 12:13:39 -0500

    7

--- grades_manager.py
+++ grades_manager.py
@@ -1,2 +1,24 @@
 def initialize_dict(student_name, subject_grades):
     return {student_name: subject_grades}
+
+def add_student(student_grades={}):
+    name = input("Enter student name: ").title()
+
+    subjects = {}
+
+    while True:
+        entry = input("Enter subject and grade (or 'exit' to finish): ")
+
+        if entry.lower() == "exit":
+            break
+
+        subject, grade = entry.split(",")
+        subject = subject.title()
+        grade = float(grade)
+
+        subjects[subject] = grade
+
+    student_grades[name] = subjects
+    print(f"Student {name} successfully added to the grades management system.")
+    return student_grades
+
