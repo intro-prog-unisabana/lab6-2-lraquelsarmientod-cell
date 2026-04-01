22814e6a5a85984b8ee663c68c362ddc7d8ca8f7
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-01 12:24:52 -0500

    12

---
+++ industrial_temperature.py
@@ -0,0 +1,7 @@
+def trigger_alarm(temperatures, threshold=80):
+    result = []
+
+    for sensor, temp in temperatures.items():
+        if temp > threshold:
+            result.append(sensor)
+    return result
